78c05304f332df7603980fff1056f628d2bbc9af
RachaelED <55112454+RachaelED@users.noreply.github.com>
2021-11-08 11:54:57 -0500

    Create titleScreen file

---
+++ titleScreen.js
@@ -0,0 +1,8 @@
+var titleScreen = function () {
+  background(0, 0, 0);
+  fill(255, 255, 255);
+  text("NAME/TITLE", 150, 150);
+  rect(100, 300, 200, 50);
+  fill(0, 0, 0);
+  text("Click to begin", 150, 325);
+};
