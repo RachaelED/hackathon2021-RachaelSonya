52d8a6422c89b27991f9abcba2fe27ba6242b6d6
RachaelED <55112454+RachaelED@users.noreply.github.com>
2021-11-09 11:57:08 -0500

    Make file object oriented

--- game.js
+++ game.js
@@ -1,14 +1,35 @@
 //create game function
-var game = function (){
-  //set background
-  background(0,0,0);
-  //Generate random numbers
-  var num1 = Math.floor(Math.random()*20);
-  var num2 = Math.floor(Math.random()*20);
-  var math1 = ["-","+"];
+var Game = function (){
+  this.num1 = 20;
+  this.num2 = 10;
   
-  fill(255,0,0);
-  text(num1,90,100);
-  text(math1[0],100,100);
-  text(num2,110,100);
-}
+  this.display = function () {
+    background(0, 0, 0);
+    fill(255, 0, 0);
+    text(this.num1 + "+" + this.num2, 100, 100);
+  };
+  
+  //this.operation = ["-", "+"];
+  //this.operation = '+';
+  
+  //text(this.num1 + this.operation + this.num2);
+  
+  
+  
+  
+  //set background
+  //background(0,0,0);
+  //Generate random numbers
+  //var num1 = Math.floor(Math.random()*20);
+  //var num2 = Math.floor(Math.random()*20);
+  //var math1 = ["-","+"];
+  
+  //var ans 
+  //while(ans!="STOP"){
+  //fill(255, 255, 255);
+  //text(num1,90,100);
+  //text(math1[0],100,100);
+  //text(num2,110,100);
+  //}
+
+};
