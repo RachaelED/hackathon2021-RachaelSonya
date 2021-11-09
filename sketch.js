63506a4e03b309feeebcc0c591b501224729e552
RachaelED <55112454+RachaelED@users.noreply.github.com>
2021-11-09 11:13:01 -0500

    Update sketch file
    Add mouseClicked function and if statements to switch between scenes

--- sketch.js
+++ sketch.js
@@ -1,8 +1,27 @@
+// keep track of current scene
+var scene = 0;
+// keep track of money donated
+var moneyDonated = 0;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  // background(220);
+  // Call title screen function
   titleScreen();
+  
+  if (scene === 0) {
+    titleScreen();
+  } else if (scene === 1) {
+    game();
+  }
 }
+
+// check if mouse is clicked
+var mouseClicked = function () {
+  // if the current scene is the title screen, advance to game scene
+  if (scene === 0 && mouseX < 300 && mouseX > 100 && mouseY < 350 && mouseY > 300) {
+    scene++;
+  }
+};
