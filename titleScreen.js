11ea4d39709aa18fe229983b249d8600a0a39a85
RachaelED <55112454+RachaelED@users.noreply.github.com>
2021-11-09 11:13:34 -0500

    Add text size commands

--- titleScreen.js
+++ titleScreen.js
@@ -1,8 +1,10 @@
 var titleScreen = function () {
   background(0, 0, 0);
   fill(255, 255, 255);
-  text("NAME/TITLE", 150, 150);
+  textSize(30);
+  text("NAME/TITLE", 120, 150);
   rect(100, 300, 200, 50);
   fill(0, 0, 0);
-  text("Click to begin", 150, 325);
+  textSize(20);
+  text("Click to begin", 135, 330);
 };
