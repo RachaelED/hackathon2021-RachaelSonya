172acb7239c821b260db07ec5e5d22c711f6c9e1
RachaelED <55112454+RachaelED@users.noreply.github.com>
2021-11-10 11:54:17 -0500

    Update sketch file

--- sketch.js
+++ sketch.js
@@ -3,10 +3,18 @@ var scene = 0;
 // keep track of money donated
 var moneyDonated = 0;
 
+// initialize input and answer variables
+var input;
+var ans;
+
+// initialize grade1 variable
 var grade1;
 
 function setup() {
   createCanvas(400, 400);
+  // create an input box
+  input = createInput();
+  // pass parameters to game function
   grade1 = new Game(1, 0, 10, ['+', '-']);
 }
 
@@ -18,6 +26,8 @@ function draw() {
     titleScreen();
   } else if (scene === 1) {
     grade1.display();
+  } else if (scene === 2) {
+    grade1.showAnswer();
   }
 }
 
@@ -27,4 +37,18 @@ var mouseClicked = function () {
   if (scene === 0 && mouseX < 300 && mouseX > 100 && mouseY < 350 && mouseY > 300) {
     scene++;
   }
+  
+  // if the current scene is the display scene
+  if (scene === 1 && mouseX < 375 && mouseX > 325 && mouseY < 350 && mouseY > 325) {
+    //rect(325, 325, 50, 25);
+    scene++;
+    // fill(0, 0, 255);
+    // rect(100, 100, 200, 200);
+    
+  }
+};
+
+// set the answer variable to be the value of the user's input
+var textInput = function () {
+  ans = input.value();
 };
