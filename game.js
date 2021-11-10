48cdc42870e2887c498d21ff471ca03db478aa07
RachaelED <55112454+RachaelED@users.noreply.github.com>
2021-11-10 09:57:24 -0500

    Add to display of game
    Make code more object oriented and reusable. Allow grade to be passed to function, along with the highest number and lowest number, and possible operations.

--- game.js
+++ game.js
@@ -1,12 +1,23 @@
 //create game function
-var Game = function (){
-  this.num1 = 20;
-  this.num2 = 10;
+// pass in values for the lowest/highest numbers and an array of possible operations
+var Game = function (g, l, h, o){
+  this.grade = g;
+  this.low = l;
+  this.high = h;
+  this.operation = o[Math.floor(random(0, o.length))];
+  this.num1 = Math.floor(random(this.low, this.high));
+  this.num2 = Math.floor(random(this.low, this.high));
   
   this.display = function () {
     background(0, 0, 0);
-    fill(255, 0, 0);
-    text(this.num1 + "+" + this.num2, 100, 100);
+    fill(255, 255, 255);
+    text("Grade " + this.grade, 10, 30);
+    textSize(15);
+    text("Type your answer to the following question in the space \nprovided.", 10, 75);
+    
+    rect(15, 125, 370, 250);
+    fill(0, 0, 0);
+    text(this.num1 + " " + this.operation + " " + this.num2 + " = ", 100, 150);
   };
   
   //this.operation = ["-", "+"];
