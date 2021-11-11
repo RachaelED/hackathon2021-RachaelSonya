8e9f7dade74e60b9a6fadf71a23edaf09259f03f
RachaelED <55112454+RachaelED@users.noreply.github.com>
2021-11-11 11:59:27 -0500

    Update image positioning

--- titleScreen.js
+++ titleScreen.js
@@ -5,15 +5,14 @@ var titleScreen = function () {
   text("Math review", 120, 150);
   rect(100, 300, 200, 40);
   noStroke();
-  rect(300,50,10,50);
-  rect(280,70,50, 10);
+  rect(340,100,10,50);
+  rect(320,120,50, 10);
   rect(160,50,50,10);
   rect(160,70,50,10);
   rect(50,70,50,10);
   rect(30,160,50,10);
   ellipse(55,140,15,15);
   ellipse(55,190,15,15);
-  fill(255,0,0);
   fill(0, 0, 0);
   textSize(20);
   text("Click to begin", 135, 330);
