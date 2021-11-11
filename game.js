dc1452c73b94deddbfafc971a7758037014c4ed6
RachaelED <55112454+RachaelED@users.noreply.github.com>
2021-11-11 10:33:31 -0500

    Add check for subtraction operation

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
 //create game function
 // pass in values for the lowest/highest numbers and an array of possible operations
-var Game = function (g, l, h, o){
+var Game = function (g, l, h, o){ 
   this.grade = g;
   this.low = l;
   this.high = h;
   this.operation = o[Math.floor(random(0, o.length))];
   this.num1 = Math.floor(random(this.low, this.high));
   this.num2 = Math.floor(random(this.low, this.high));
+  
+  while (this.num1 > this.num2) {
+    this.num1 = Math.floor(random(this.low, this.high));
+  }
   
   this.display = function () {
     background(0, 0, 0);
@@ -31,24 +35,50 @@ var Game = function (g, l, h, o){
     fill(255, 255, 255);
     text("Check", 325, 345);
     
-    fill(0, 0, 0);
-    text(scene, 200, 200);
+    fill(255, 0, 0);
+    text(moneyDonated, 250, 25);
     
   };
   
   this.showAnswer = function () {
-   
-    // background(255, 0, 0);
     
+    // redraw background
+    background(0, 0, 0);
+    // display question
+    fill(255, 255, 255);
+    text(this.num1 + " " + this.operation + " " + this.num2 + " = ", 100, 150);
+    
+    // convert the answer to be stored as a number instead of as a string
+    ans = parseInt(ans, 10);
+    
+    // if the operation is a +, then add the two numbers and display the answer
     if (this.operation === '+') {
       fill(0, 255, 0);
-      text(this.num1 + this.num2, 250, 200);
+      text(this.num1 + this.num2, 200, 150);
       if (ans === this.num1 + this.num2) {
         fill(0, 0, 255);
-        text("Correct!", 200, 200);
-        text(this.num1 + this.num2, 250, 200);
+        text("Correct! You donated 2 cents!", 150, 225);
+        moneyDonated += 0.02;
+        text(moneyDonated, 100, 300);
+        //text(this.num1 + this.num2, 250, 200);
+      } else if (ans != this.num1 + this.num2) {
+        text("Incorrect.", 150, 225);
       }
     }
+    
+    // if the operation is a -, then subtract the two numbers and display the answer
+    if (this.operation === '-') {
+      fill(0, 255, 0);
+      text(this.num1 - this.num2, 200, 150);
+      if (ans === this.num1 - this.num2) {
+        fill(0, 0, 255);
+        text("Correct! You donated 2 cents!", 150, 225);
+        moneyDonated += 0.02;
+      } else if (ans != this.num1 - this.num2) {
+        text("Incorrect.", 150, 225);
+      }
+    }
+    
   };
   
 
