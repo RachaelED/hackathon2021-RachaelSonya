393d614f6bbb96e4b0d377b85bc1b420ad234ec4
RachaelED <55112454+RachaelED@users.noreply.github.com>
2021-11-25 18:03:55 -0500

    Fix some formatting issues

--- game.js
+++ game.js
@@ -27,7 +27,7 @@ var Game = function (g, l, h, o){
     fill(0, 0, 0);
     textSize(30);
     // display the question
-    text(this.num1 + " " + this.operation + " " + this.num2 + " = ", 100, 200);
+    text(this.num1 + " " + this.operation + " " + this.num2 + " = ", 75, 200);
     textSize(16);
     
     // if the input value has changed, call the textInput function
@@ -35,7 +35,7 @@ var Game = function (g, l, h, o){
     
     fill(255, 255, 255);
     // use Math.round(_ * 100) / 100 to round to 2 decimal places
-    text("Total: " + Math.round(moneyDonated * 100)/100, 237, 40);
+    text("Total: " + Math.round(moneyDonated * 100)/100, 232, 40);
     
   };
   
